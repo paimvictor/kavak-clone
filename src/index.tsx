30071971a0278de83e93646f588c2b393bd94c72
Victor Paim <victorspaim@hotmail.com>
2022-11-29 13:16:06 -0300

    feat: Adds React Router

--- src/index.tsx
+++ src/index.tsx
@@ -2,14 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+} from "react-router-dom";
+import Root from "./routes/root";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+  },
+]);
+
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
+    <RouterProvider router={router} />
     <App />
     
   </React.StrictMode>
 );
-
